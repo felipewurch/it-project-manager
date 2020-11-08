c6151bcbf3e7fa35112a6560c211025310e6b683
Felipe Wurch <felipewurch@gmail.com>
2020-11-08 13:16:39 -0300

    sla

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ typedef struct pes {
 typedef struct tar {
   char tituloTarefa[40];
   char descricao[150];
-  unsigned float tempoEstimado;
+  float tempoEstimado;
   unsigned int tipoTarefa;
   char status[10];
 } tarefa;
@@ -26,6 +26,25 @@ typedef struct proj {
 
 int main()
 {
+
+
+  struct proj *ptr;
+   int i, n;
+
+   printf("Enter the number of projects: ");
+   scanf("%d", &n);
+
+  
+   ptr = (struct proj*) malloc(n * sizeof(struct proj));
+
+   for(i = 0; i < n; ++i)
+   {
+       printf("Enter project name: ");
+       scanf("%d ", &(ptr+i)->nomeProjeto);
+       
+       
+   }
+
 
 return 0;
 }
